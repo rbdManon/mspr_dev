0251a3a7dcfce00d4317f776496c7ae174552732
Manon-Rbd <rambau.manon@gmail.com>
2019-05-20 08:00:12 +0200

    MRA : begin calculator

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import sha256 from 'sha256';
+import { HttpClient, HttpParams } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,14 +10,21 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
   formLogin : FormGroup;
-  constructor(public navCtrl : NavController, public formBuilder: FormBuilder) {
+  constructor(public navCtrl : NavController, public formBuilder: FormBuilder, public http : HttpClient) {
      // Create the form and define fields and validators.
      this.formLogin = this.formBuilder.group({
       login:['', Validators.required],
       password:['', Validators.required],
     });
    }
-  signIn(){
+  signIn(mdo:NgForm){
+    let login = mdo.value.login;
+    let password = sha256(mdo.value.password)
+    console.log('log : '+login + '  pass : ' + password );
+    // this.http.get('http://localhost:8080/dmoes/search/findByLoginAndPassword',
+    // {'email':login, 'password':sha256(password)},
+  
+    // );
     if (this.formLogin.valid) {
             this.navCtrl.navigateForward('home');
     }else{
@@ -27,6 +36,7 @@ export class LoginPage implements OnInit {
   }
 
   ngOnInit() {
+    console.log(sha256('lucas'))
   }
 
 }
