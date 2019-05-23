174d6538193bf07baa94ca82b87c9b3f072e800c
Charly85_ <caillon.charly@gmail.com>
2019-05-24 00:55:28 +0200

    Http pending progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LoadingPageModule, MaterialBarModule  } from 'angular-loading-page';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpClientModule,
+    LoadingPageModule,
+    MaterialBarModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
