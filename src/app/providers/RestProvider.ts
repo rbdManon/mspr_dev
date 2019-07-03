61df56ea30ac46b63dffd03844619fe3b3416013
Charly85_ <caillon.charly@gmail.com>
2019-07-03 22:27:08 +0200

    Sauvegarder toutes les requetes en localStorage

--- src/app/providers/RestProvider.ts
+++ src/app/providers/RestProvider.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { reject } from 'q';
+import LocalStorage from '../utils/LocalStorage';
 
 @Injectable({
     providedIn: 'root'
@@ -11,7 +12,22 @@ export class RestProvider {
     ) { }
 
     public get(url: string) {
-        return this.HttpClient.get(url).toPromise()
+        return new Promise((resolve, reject) => {
+            this.HttpClient.get(url).toPromise()
+            .then(res => {
+                LocalStorage.set(url, res)
+                resolve(res)
+            })
+            .catch(err => {
+                let db_stored = LocalStorage.get(url)
+                if(db_stored !== null) {
+                    resolve(db_stored)
+                }
+                else {
+                    reject(err)
+                }
+            })
+        })
     }
 
     public post(url: string, elem) {
